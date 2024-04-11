f72e29a78a695d9ae13f8edd388ce295b132bd85
bradyz314 <bradyz@seas.upenn.edu>
2024-04-11 16:11:04 -0400

    Add eventlistener to detect booking page

--- public/contentScript.js
+++ public/contentScript.js
@@ -1,3 +1,13 @@
-document.addEventListener('mousedown', () => {
-    alert('Pressed');
-})
+window.addEventListener('popstate', () => { 
+    // Get the current URL.
+    const new_url = window.location.href;
+    // Define regex pattern to detect the flights booking page.
+    const booking_regex = /.*\.google\.com\/travel\/flights\/booking*/;
+    // If the URL is the booking page, begin scraping the data.
+    if (booking_regex.test(new_url)) {
+        console.log("Begin webscraping.");
+    } else {
+        console.log("No match", new_url);
+    }
+
+});
