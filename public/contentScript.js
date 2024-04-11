d91a70b562f0583e5c6a335f71c29707f8def8bb
bradyz314 <bradyz@seas.upenn.edu>
2024-04-11 18:23:58 -0400

    Use fetch to grab document rather than using window.document in scrape_booking function

--- public/contentScript.js
+++ public/contentScript.js
@@ -1,13 +1,34 @@
-window.addEventListener('popstate', () => { 
-    // Get the current URL.
-    const new_url = window.location.href;
-    // Define regex pattern to detect the flights booking page.
-    const booking_regex = /.*\.google\.com\/travel\/flights\/booking*/;
-    // If the URL is the booking page, begin scraping the data.
-    if (booking_regex.test(new_url)) {
-        console.log("Begin webscraping.");
-    } else {
-        console.log("No match", new_url);
-    }
+console.log('injected')
 
+// Helper function that will scrape relevant data from booking page
+function scrape_booking(url) {
+    // Regex expression to check if provided URL corresponds to a Google Flights booking page
+    const booking_regex = /.*\.google\.com\/travel\/flights\/booking*/;
+    // If the URL is the booking page, scrape the data.
+    if (booking_regex.test(url)) {
+        // Fetch the document content asyncronously
+        fetch(url)
+        .then(response => response.text())
+        .then(html => {
+            // Parse the HTML string into a DOM object
+            const parser = new DOMParser();
+            const doc = parser.parseFromString(html, 'text/html');
+            
+            // Now you can access the current document object (doc)
+            const divs = doc.querySelectorAll('div')
+            console.log(divs.length)
+            // Your scraping logic here
+        })
+        .catch(error => {
+            console.error('Error fetching document:', error);
+        });
+    }
+}
+
+// Call scrape_booking on injection in case the user has directly clicked on the booking page
+scrape_booking(window.location.href);
+// Add an event listener that will call scrape_booking on the popstate event
+window.addEventListener('popstate', () => { 
+    // Fetch the current document content asynchronously
+    scrape_booking(window.location.href);
 });
