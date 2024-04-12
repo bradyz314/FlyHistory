a8235e6c755d3900579d5e73868d365fa02bf28e
bradyz314 <bradyzhou314159@gmail.com>
2024-04-12 00:41:39 -0400

    Add scraping logic for origin, destination, and is_round_trip

--- public/contentScript.js
+++ public/contentScript.js
@@ -4,6 +4,8 @@ console.log('injected')
 function scrape_booking(url) {
     // Regex expression to check if provided URL corresponds to a Google Flights booking page
     const booking_regex = /.*\.google\.com\/travel\/flights\/booking*/;
+    const one_way_regex = /(.*) to (.*)/;
+    const round_trip_regex = /(.*) to (.*) and back/;
     // If the URL is the booking page, scrape the data.
     if (booking_regex.test(url)) {
         // Fetch the document content asyncronously
@@ -13,11 +15,18 @@ function scrape_booking(url) {
             // Parse the HTML string into a DOM object
             const parser = new DOMParser();
             const doc = parser.parseFromString(html, 'text/html');
-            
-            // Now you can access the current document object (doc)
-            const divs = doc.querySelectorAll('div')
-            console.log(divs.length)
-            // Your scraping logic here
+            // First, scrape the flight's starting and end points (ignore multi-city trips for now)
+            const location_div_label = Array.from(doc.querySelectorAll('*')).find(element => 
+                element.getAttribute('aria-label')?.toLowerCase().includes('to')
+            ).getAttribute('aria-label');
+            // Match label on one_way_regex and round_trip_regex
+            const one_way_match = location_div_label.match(one_way_regex);
+            const round_trip_match = location_div_label.match(round_trip_regex);
+            const is_round_trip = round_trip_match !== null;
+            // Extract capture groups from the corresponding regex pattern
+            const origin = is_round_trip ? round_trip_match[1] : one_way_match[1];
+            const destination = is_round_trip ? round_trip_match[2] : one_way_match[2];
+            console.log(is_round_trip, origin, destination);
         })
         .catch(error => {
             console.error('Error fetching document:', error);
