65a05d2fb0a6e60e4efaaca53dbb15920226ffa0
bradyz314 <bradyzhou314159@gmail.com>
2024-04-14 20:57:31 -0400

    Extract stop information and store flight information in an array

--- public/contentScript.js
+++ public/contentScript.js
@@ -4,7 +4,8 @@ function scrape_booking(url) {
     const booking_regex = /.*\.google\.com\/travel\/flights\/booking*/;
     const one_way_regex = /(.*) to (.*)/;
     const round_trip_regex = /(.*) to (.*) and back/;
-    const date_regex = /^(\w{3}), (\w{3}) (\d*)$/
+    const date_regex = /^(\w{3}), (\w{3}) (\d*)$/;
+    const stop_regex = /(\d*) stop[s]? flight/;
     // If the URL is the booking page, scrape the data.
     if (booking_regex.test(url)) {
         // Get the text related divs in the document
@@ -13,13 +14,15 @@ function scrape_booking(url) {
         const location_div_label = text_divs[0].getAttribute('aria-label');
         // Get the nested span containing the price of the flights
         const price_span_content = text_divs[1].querySelector('div span').textContent;
-        // Get the divs that contain the airport codes corresponding to the flights
-        const airport_code_divs = Array.from(document.querySelectorAll('div')).filter(element => element.className === '' && element.length === 3);
+        // Get the divs that contain the airport codes corresponding to the flights (every 2 divs refer to the same flight)
+        const airport_code_divs = Array.from(document.querySelectorAll('div')).filter(element => element.className === '' && element.textContent.length === 3);
         // Get the divs that contain the departure and arrival times corresponding to the flights (every 6 divs refer to the same flight -> only use first 2 of every 6)
         const flight_time_divs = Array.from(document.querySelectorAll('div')).filter(element => element.getAttribute('aria-label') !== null && element.getAttribute('aria-label').includes('time'));
-        // Get the divs that contain the day of the flights
+        // Get the divs that contain the day of the flights (every div refers to one flight)
         const flight_date_divs = Array.from(document.querySelectorAll('div')).filter(element => date_regex.test(element.textContent))
-        const flight_info = {}
+        // Get the spans that contain the number of stops for each flight
+        const flight_stop_spans = Array.from(document.querySelectorAll('span')).filter(element => stop_regex.test(element.getAttribute('aria-label')));
+        const flight_info = []
         // For now, ignore multi-city trips
         if (location_div_label !== null) {
             // Match label on one_way_regex and round_trip_regex
@@ -29,7 +32,18 @@ function scrape_booking(url) {
             // Extract capture groups from the corresponding regex pattern
             const origin = is_round_trip ? round_trip_match[1] : one_way_match[1];
             const destination = is_round_trip ? round_trip_match[2] : one_way_match[2];
+            for (let i = 0; i < flight_date_divs.length; i++) {
+                flight_info.push({
+                    'date': flight_date_divs[i].textContent,
+                    'origin_airport': airport_code_divs[2 * i].textContent,
+                    'destination_airport': airport_code_divs[2 * i + 1].textContent,
+                    'departure_time': flight_time_divs[6 * i].textContent,
+                    'arrival_time': flight_time_divs[6 * i + 1].textContent,
+                    'num_stops': flight_stop_spans[i].textContent
+                });
+            }
             console.log(is_round_trip, origin, destination, price_span_content);
+            console.log(flight_info);
         }
     }
 }
