59e78971cbf2357e93d4246f507ed5b39bdce585
melaniechen912 <melaniechen864@gmail.com>
2024-04-23 16:58:53 -0400

    working on getting webscraped data to show up on sidebar, have errors now tho

--- public/flightList.js
+++ public/flightList.js
@@ -7,43 +7,19 @@ function getAllTrips() {
 
 const allTrips = getAllTrips();
 
-const testFlights = [
-    {
-        airline: 'Frontier',
-        origin: 'PHL',
-        destination: 'MCO',
-        departureTime: '8:45 AM',
-        arrivalTime: '11:34 AM',
-        duration: '2 hr 49 min',
-        stops: 'Nonstop',
-        price: 500,
-        num_stops: 3
-    },
-    {
-        airline: 'Frontier',
-        origin: 'PHL',
-        destination: 'MCO',
-        departureTime: '3:10 PM',
-        arrivalTime: '6:00 PM',
-        duration: '2 hr 50 min',
-        stops: 'Nonstop',
-        price: 700,
-        num_stops: 1
-
-    },
-    {
-        airline: 'Spirit',
-        origin: 'PHL',
-        destination: 'MCO',
-        departureTime: '2:12 PM',
-        arrivalTime: '4:55 PM',
-        duration: '2 hr 43 min',
-        stops: 'Nonstop',
-        price: 250,
-        num_stops: 0,
-        note: 'SEPARATE TICKETS BOOKED TOGETHER'
-    }
-];
+// const testFlights = [
+//     {
+//         airline: 'Frontier',
+//         origin: 'PHL',
+//         destination: 'MCO',
+//         departureTime: '8:45 AM',
+//         arrivalTime: '11:34 AM',
+//         duration: '2 hr 49 min',
+//         stops: 'Nonstop',
+//         price: 500,
+//         num_stops: 3
+//     },
+// ];
 
 function createFlightInfoElement(flight) {
     const flightInfoElement = document.createElement('div');
@@ -53,13 +29,14 @@ function createFlightInfoElement(flight) {
     detailsDiv.classList.add('details');
 
     const timeDiv = document.createElement('div');
-    timeDiv.textContent = `${flight.departureTime} ➡ ${flight.arrivalTime}`;
+    // timeDiv.textContent = `${flight.departureTime} ➡ ${flight.arrivalTime}`;
+    timeDiv.textContent = `${flight.flights}`;
 
     const airportDiv = document.createElement('div');
     airportDiv.textContent = `${flight.origin} ${flight.destination}`;
 
     const durationDiv = document.createElement('div');
-    durationDiv.textContent = `${flight.stops} · ${flight.duration} · ${flight.airline}`;
+    // durationDiv.textContent = `${flight.stops} · ${flight.duration} · ${flight.airline}`;
 
     if (flight.note) {
         const noteDiv = document.createElement('div');
@@ -74,15 +51,13 @@ function createFlightInfoElement(flight) {
     const priceStopsContainer = document.createElement('div');
     priceStopsContainer.classList.add('price-stops-container');
 
-
-
     const priceDiv = document.createElement('div');
     priceDiv.classList.add('price');
-    priceDiv.textContent = `$${flight.price}`;
+    priceDiv.textContent = `${flight.price}`;
 
     const stopsDiv = document.createElement('div');
-    stopsDiv.classList.add('num_stops');
-    stopsDiv.textContent = `${flight.num_stops} stop(s)`;
+    stopsDiv.classList.add('flight_type');
+    stopsDiv.textContent = `${flight.flight_type}`;
 
     priceStopsContainer.appendChild(priceDiv);
     priceStopsContainer.appendChild(stopsDiv);
@@ -93,45 +68,40 @@ function createFlightInfoElement(flight) {
     return flightInfoElement;
 }
 
-function updateOrder(price, stops, order) {
+function updateOrder(allTrips, price, stops, order) {
 
     if (order == 'price-asc') {
-        testFlights.sort((a, b) => {
+        allTrips.sort((a, b) => {
               return a.price - b.price;
             });
     } else if (order === 'price-desc') {
-        testFlights.sort((a, b) => {
+        allTrips.sort((a, b) => {
             return b.price - a.price;
           });
     } else if (order === 'stops-asc') {
-        testFlights.sort((a, b) => {
+        allTrips.sort((a, b) => {
             return a.num_stops - b.num_stops;
           });
     } else if (order === 'stops-desc') {
-        testFlights.sort((a, b) => {
+        allTrips.sort((a, b) => {
             return b.num_stops - a.num_stops;
           });
     }
 
     renderFlights(price, stops);
-
-
-
     
 }
 
-function renderFlights(maxPrice, maxStops) {
-    
-    
+function renderFlights(allTrips) {
     flightList.innerHTML = '';
-
-    testFlights.forEach(flight => {
-        if (flight.price <= maxPrice && flight.num_stops <= maxStops) {
-            const flightInfoElement = createFlightInfoElement(flight);
-            flightList.appendChild(flightInfoElement);
-        }
+    allTrips.forEach(flight => {
+        const flightInfoElement = createFlightInfoElement(flight);
+        flightList.appendChild(flightInfoElement);
     });
 }
+
+renderFlights(allTrips);
+
 /*
 
 function renderFlightsByPrice(maxPrice) {
@@ -156,7 +126,3 @@ function renderFlightsByStops(maxStops) {
     });
 }
 */
-
-
-renderFlights(1000, 5);
-
