db2994688edd5c6b94fac9517982eeddce8cd259
bradyz314 <bradyzhou314159@gmail.com>
2024-05-03 23:12:04 -0400

    Update trip to have buttons

--- src/components/TripItem.tsx
+++ src/components/TripItem.tsx
@@ -1,3 +1,5 @@
+import { Box, Button } from '@mui/material';
+
 export interface FlightData {
     date: string,
     origin_airport: string,
@@ -12,6 +14,7 @@ export interface TripData {
     flight_type: string,
     origin: string,
     destination: string,
+    class: string,
     price: string,
     flights: FlightData[],
     flight_url: string
@@ -23,24 +26,42 @@ export interface TripItemProps {
 } 
 
 export default function TripItem({tripData, onDelete}: TripItemProps) {
-    console.log(tripData);
     return (
-        <div className='trip-info'>
-            <h3 className='trip-type'>{tripData.flight_type === 'Multi-City Trip' ? tripData.flight_type : `${tripData.origin} to ${tripData.destination}`}</h3>
-            <p className='trip-cost'>{`Cost: ${tripData.price}`}</p>
-            {tripData.flights.map((data, idx) => (
+        <div className='trip-info' style={{border: '3px solid #4387cc', borderRadius: '5px', margin: '5px 0px', padding: '5px'}}>
+             <h2 className='trip type'>{tripData.flight_type === 'Multi-City Trip' ? tripData.flight_type : `${tripData.origin} to ${tripData.destination}`}</h2>
+             <h4 className='trip cost'>{`Cost: ${tripData.price}`}</h4>
+             <h4 className='trip class'>{`Class: ${tripData.class}`}</h4>
+             {tripData.flights.map((data, idx) => (
                 <div className='flight-info' key={idx}>
-                    <h5 className='flight-no'>{idx}</h5>
+                    <h3 className='flight-no'>{`Flight #${idx + 1}`}</h3>
                     <p className='flight-date'>{`Data: ${data.date}`}</p>
                     <p className='flight-airport'>{`Origin: ${data.origin_airport}`}</p>
                     <p className='flight-departure'>{`Depart Time: ${data.departure_time}`}</p>
                     <p className='flight-airport'>{`Destination: ${data.destination_airport}`}</p>
                     <p className='flight-arrival'>{`Arrival Time: ${data.arrival_time}`}</p>
-                    <p className='flight-no-of-stops'>{`Stops: data.num_stops`}</p>
+                    <p className='flight-no-of-stops'>{`Stops: ${data.num_stops}`}</p>
                 </div>
             ))}
-            <a href={tripData.flight_url} className='trip-link'>Click here for more.</a>
-            <button className='trip-delete' onClick={() => onDelete(tripData.id)}>Remove</button>
+             <Box textAlign='center'>
+                <Button 
+                    variant="contained"
+                    onClick={() => {
+                        chrome.tabs.create({url: tripData.flight_url});
+                    }}
+                    className="trip link-btn"
+                    color="success"
+                >
+                    Redirect
+                </Button>
+                <Button 
+                    variant="contained"
+                    onClick={() => onDelete(tripData.id)}
+                    className="trip filter-btn"
+                    color="error"
+                >
+                    Remove
+                </Button>
+             </Box>
         </div>
     );
 }
