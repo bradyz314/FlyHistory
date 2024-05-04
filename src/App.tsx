86640770f80ea9114d1480ea53a54731f8d3de3a
bradyz314 <bradyzhou314159@gmail.com>
2024-05-03 23:11:33 -0400

    Add filtering for origin, destination, number of flights, and flight class

--- src/App.tsx
+++ src/App.tsx
@@ -2,19 +2,41 @@ import './App.css';
 import { useState, useEffect } from 'react';
 import { TripData } from './components/TripItem';
 import TripList from './components/TripList';
+import FilterBar from './components/FilterBar';
 
 
 function App() {
   const [trips, setTrips] = useState<TripData[]>([]);
+  const [origin, setOrigin] = useState('');
+  const [destination, setDestination] = useState('');
+  const [flights, setFlights] = useState(-1);
+  const [flightClass, setFlightClass] = useState('Any');
   // UseEffect to load trips from local storage
   // Use useEffect to load snippets from local storage when the component mounts
   useEffect(() => {
     chrome.storage.local.get({'flights': []}, (result) => {
-      setTrips(result.flights);
+      let trips = result.flights;
+      // Filter by origin if necessary
+      if (origin.length > 0) {
+        trips = trips.filter((flight: TripData) => flight.origin === origin);
+      }
+      // Filter by destination if necessary
+      if (destination.length > 0) {
+        trips = trips.filter((flight: TripData) => flight.destination === destination);
+      }
+      // Filter by number of flights if necessary
+      if (flights !== -1) {
+        trips = trips.filter((flight: TripData) => flight.flights.length === flights);
+      }
+      // Filter by flight class if necessary
+      if (flightClass !== 'Any') {
+        trips = trips.filter((flight: TripData) => flight.class === flightClass);
+      }
+      setTrips(trips);
     });
-  }, []);
-  // Handler for deleting a trip from the list
-  const handleDelete = (id: number) => {
+  }, [destination, origin, flights, flightClass]);
+    // Handler for deleting a trip from the list
+    const handleDelete = (id: number) => {
     // Create a new array without the deleted trip
     const filtered = trips.filter((trip) => trip.id !== id);
     // Update the state with the new array
@@ -25,7 +47,8 @@ function App() {
 
   return (
     <div>
-      <h1> FlyHistory </h1>
+      <h1 className='header'> FlyHistory </h1>
+      <FilterBar handleOriginChange={setOrigin} handleDestinationChange={setDestination} handleFlightsChange={setFlights} handleClassChange={setFlightClass}/>
       <TripList trips={trips} onDelete={handleDelete}/>
     </div>
   );
--- src/components/FilterBar.tsx
+++ src/components/FilterBar.tsx
@@ -1,16 +1,43 @@
-import { Box, Button, TextField } from "@mui/material";
+import { Box, Button, FormControl, InputLabel, MenuItem, Select, TextField, outlinedInputClasses, selectClasses } from "@mui/material";
 import Grid from '@mui/material/Unstable_Grid2';
 import { useState } from "react";
+import { styled } from '@mui/system';
 
 
 interface FilterBarProps {
     handleOriginChange: (origin: string) => void,
     handleDestinationChange: (destination: string) => void,
+    handleFlightsChange: (flights: number) => void,
+    handleClassChange: (flightClass: string) => void
 }
 
-export default function FilterBar({handleOriginChange, handleDestinationChange} : FilterBarProps) {
+const StyledSelect = styled(Select)`
+    border-color: #4387cc;
+    color: white
+
+    & .${selectClasses.icon} {
+    color: #4387cc;
+    }
+
+    & .${outlinedInputClasses.notchedOutline} {
+    border-color: #4387cc;
+    }
+    &:hover .${outlinedInputClasses.notchedOutline} {
+    border-color: #4387cc;
+    }
+
+    &.${outlinedInputClasses.focused} .${outlinedInputClasses.notchedOutline} 
+    {
+    // VERY IMPORTANT TO NOT LEAVE AN EMPTY SPACE BETWEEN '&' AND '.'
+    border-color: #4387cc !important;
+    }
+    `;
+
+export default function FilterBar({handleOriginChange, handleDestinationChange, handleFlightsChange, handleClassChange} : FilterBarProps) {
     const [origin, setOrigin] = useState('');
     const [destination, setDestination] = useState('');
+    const [flights, setFlights] = useState(-1);
+    const [flightClass, setFlightClass] = useState('Any');
     return (
         <Grid container spacing={2}>
             <Grid xs={12}>
@@ -22,7 +49,6 @@ export default function FilterBar({handleOriginChange, handleDestinationChange} 
                     sx={{
                         input: { 
                             color: 'white',  
-                            borderColor: '#4387cc'
                         }
                     }}
                     onChange={(e) => setOrigin(e.target.value)}
@@ -34,14 +60,51 @@ export default function FilterBar({handleOriginChange, handleDestinationChange} 
                     label="Destination"
                     focused
                     fullWidth
+                    variant="outlined"
                     sx={{
                         input: { 
                             color: 'white',  
-                            borderColor: '#4387cc'
                         }
                     }}
                     onChange={(e) => setDestination(e.target.value)}
                 />
+            </Grid>
+            <Grid xs={6}>
+                <FormControl fullWidth>
+                    <InputLabel id="flight-select" style={{color: '#4387cc'}}>Num. of Flights</InputLabel>
+                    <StyledSelect
+                        labelId="demo-simple-select-label"
+                        id="demo-simple-select"
+                        value={flights}
+                        label="Num. of Flights"
+                        style={{color: 'white'}}
+                        onChange={(e) => setFlights(e.target.value as number)}
+                    >
+                        <MenuItem value={-1}>Any</MenuItem>
+                        <MenuItem value={1}>One</MenuItem>
+                        <MenuItem value={2}>Two</MenuItem>
+                        <MenuItem value={3}>Three</MenuItem>
+                        <MenuItem value={4}>Four</MenuItem>
+                        <MenuItem value={5}>Five</MenuItem>
+                    </StyledSelect>
+                </FormControl>
+            </Grid>
+            <Grid xs={6}>
+                <FormControl fullWidth>
+                    <InputLabel style={{color: '#4387cc'}}>Flight Class</InputLabel>
+                    <StyledSelect
+                        value={flightClass}
+                        label="Flight Class"
+                        style={{color: 'white'}}
+                        onChange={(e) => setFlightClass(e.target.value as string)}
+                    >
+                        <MenuItem value={'Any'}>Any</MenuItem>
+                        <MenuItem value={'Economy'}>Economy</MenuItem>
+                        <MenuItem value={'Premium economy'}>Premium economy</MenuItem>
+                        <MenuItem value={'Business'}>Business</MenuItem>
+                        <MenuItem value={'First'}>First</MenuItem>
+                    </StyledSelect>
+                </FormControl>
             </Grid>
             <Grid xs={4}></Grid>
             <Grid xs={4}>
@@ -51,13 +114,10 @@ export default function FilterBar({handleOriginChange, handleDestinationChange} 
                         onClick={() => {
                             handleOriginChange(origin);
                             handleDestinationChange(destination);
+                            handleFlightsChange(flights);
+                            handleClassChange(flightClass);
                         }}
                         className="filter-btn"
-                        sx={{
-                            input: { 
-                                mx: 'auto'
-                            }
-                        }}
                     >
                         Filter
                     </Button>
